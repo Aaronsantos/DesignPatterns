80c16ad04bb0e31c99ad6eec7e9a293319b9cb44
Aaron Santos <aaronpedro16@gmail.com>
2018-12-17 13:31:15 -0300

    Adicionando projeto exemplo do seminário

---
+++ Seminário PWA /app/public/initFirebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyACNC5G_STLitNtdm2C4u656Uzx8wQLmWQ",
+authDomain: "padroes-projeto-pwa.firebaseapp.com",
+databaseURL: "https://padroes-projeto-pwa.firebaseio.com",
+projectId: "padroes-projeto-pwa",
+storageBucket: "padroes-projeto-pwa.appspot.com",
+messagingSenderId: "535277827262"
+  };
+  firebase.initializeApp(config);
